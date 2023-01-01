7af9c7ed81dbc1db8348381acbbd38068a63999b
Outkarm <jkpordje@gmail.com>
2023-01-01 23:53:53 +0000

    fix linter errors

--- src/modules/get.js
+++ src/modules/get.js
@@ -1,27 +1,27 @@
 const getScores = async () => {
   const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GUjrQJIaA6uCHpsxgcul/scores/"
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GUjrQJIaA6uCHpsxgcul/scores/',
   );
 
   const data = await response.json();
 
-  const scoreBoard = document.getElementById("container");
+  const scoreBoard = document.getElementById('container');
   const createIScores = (data) => {
     data.result.forEach((e) => {
-      const personScore = document.createElement("div");
-      personScore.classList.add("score-cont");
+      const personScore = document.createElement('div');
+      personScore.classList.add('score-cont');
       scoreBoard.append(personScore);
-      const personImage = document.createElement("img");
-      personImage.src = "https://img.icons8.com/bubbles/512/star.png";
-      personImage.classList.add("person-image");
+      const personImage = document.createElement('img');
+      personImage.src = 'https://img.icons8.com/bubbles/512/star.png';
+      personImage.classList.add('person-image');
       personScore.append(personImage);
-      const playerName = document.createElement("p");
+      const playerName = document.createElement('p');
       playerName.innerText = `${e.user}`;
-      playerName.classList.add("player-name");
+      playerName.classList.add('player-name');
       personScore.append(playerName);
-      const playerScore = document.createElement("p");
+      const playerScore = document.createElement('p');
       playerScore.innerText = `${e.score}`;
-      personScore.classList.add("player-score");
+      personScore.classList.add('player-score');
       personScore.append(playerScore);
     });
   };
